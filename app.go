fc105f2603c798e9becf254439c2c785b12c9e1b
Emil Guseynov <theluckyemil@gmail.com>
2020-03-24 18:42:41 +0300

    remove tabs

--- app.go
+++ app.go
@@ -3,15 +3,14 @@ package main
 import (
 	"encoding/json"
 	"github.com/rs/zerolog"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"fmt"
 	"github.com/eoscanada/eos-go/ecc"
 	"github.com/gorilla/mux"
-	"fmt"
 	"github.com/rs/zerolog/log"
 )
 
@@ -20,105 +19,105 @@ type Request = http.Request
 type JsonResponse = map[string]interface{}
 
 type App struct {
-	Router *mux.Router
-	PrivateKey *ecc.PrivateKey
-	TopicOffsetPath string
+    Router *mux.Router
+    PrivateKey *ecc.PrivateKey
+    TopicOffsetPath string
 }
 
 func (app *App) Initialize(pk *ecc.PrivateKey, offsetPath string, level string) {
-	app.Router = mux.NewRouter()
-	app.PrivateKey = pk
-	app.TopicOffsetPath = offsetPath
+    app.Router = mux.NewRouter()
+    app.PrivateKey = pk
+    app.TopicOffsetPath = offsetPath
 
-	InitLogger(level)
-	app.InitializeRoutes()
+    InitLogger(level)
+    app.InitializeRoutes()
 }
 
 func (app *App) Run(addr string) {
-	log.Error().Msg(http.ListenAndServe(addr, app.Router).Error())
+    log.Error().Msg(http.ListenAndServe(addr, app.Router).Error())
 }
 
 func respondWithError(writer ResponseWriter, code int, message string) {
-	respondWithJSON(writer, code, JsonResponse{"error": message})
+    respondWithJSON(writer, code, JsonResponse{"error": message})
 }
 
 func respondWithJSON(writer ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(code)
-	writer.Write(response)
+    response, _ := json.Marshal(payload)
+    writer.Header().Set("Content-Type", "application/json")
+    writer.WriteHeader(code)
+    writer.Write(response)
 }
 
 func (app *App) PingQuery(writer ResponseWriter, req *Request) {
-	log.Info().Msg("Called /ping")
-	respondWithJSON(writer, http.StatusOK, JsonResponse{"result":"pong"})
+    log.Info().Msg("Called /ping")
+    respondWithJSON(writer, http.StatusOK, JsonResponse{"result":"pong"})
 }
 
 func (app *App) SignQuery(writer ResponseWriter, req *Request) {
-	log.Info().Msg("Called /sign_transaction")
-	// TODO
+    log.Info().Msg("Called /sign_transaction")
+    // TODO
 }
 
 func (app *App) InitializeRoutes() {
-	app.Router.HandleFunc("/ping", app.PingQuery).Methods("GET")
-	app.Router.HandleFunc("/sign_transaction", app.SignQuery).Methods("POST")
+    app.Router.HandleFunc("/ping", app.PingQuery).Methods("GET")
+    app.Router.HandleFunc("/sign_transaction", app.SignQuery).Methods("POST")
 }
 
 func getLevel(level string) zerolog.Level {
-	switch strings.ToLower(level) {
-	case "debug":
-		return zerolog.DebugLevel
-	case "info":
-		return zerolog.InfoLevel
-	case "warning":
-		return zerolog.WarnLevel
-	case "error":
-		return zerolog.ErrorLevel
-	default:
-		return zerolog.InfoLevel
-	}
+    switch strings.ToLower(level) {
+    case "debug":
+        return zerolog.DebugLevel
+    case "info":
+        return zerolog.InfoLevel
+    case "warning":
+        return zerolog.WarnLevel
+    case "error":
+        return zerolog.ErrorLevel
+    default:
+        return zerolog.InfoLevel
+    }
 }
 
 func InitLogger(level string) {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+    log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	output.FormatLevel = func(i interface{}) string {
-		const (
-			colorBlack = iota + 30
-			colorRed
-			colorGreen
-			colorYellow
-			colorBlue
-			colorMagenta
-			colorCyan
-			colorWhite
+    output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+    output.FormatLevel = func(i interface{}) string {
+        const (
+            colorBlack = iota + 30
+            colorRed
+            colorGreen
+            colorYellow
+            colorBlue
+            colorMagenta
+            colorCyan
+            colorWhite
 
-			colorBold     = 1
-			colorDarkGray = 90
-		)
-		var colorMap = map[string]int {
-			"debug": colorYellow,
-			"info": colorGreen,
-			"warn": colorBlue,
-			"error": colorRed,
-		}
-		colorize := func(s string, c int) string {
-			return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
-		}
-		level, _ := i.(string)
-		return colorize(strings.ToUpper(level), colorMap[level])
+            colorBold     = 1
+            colorDarkGray = 90
+        )
+        var colorMap = map[string]int {
+            "debug": colorYellow,
+            "info": colorGreen,
+            "warn": colorBlue,
+            "error": colorRed,
+        }
+        colorize := func(s string, c int) string {
+            return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
+        }
+        level, _ := i.(string)
+        return colorize(strings.ToUpper(level), colorMap[level])
 
-	}
-	output.FormatFieldName = func(i interface{}) string {
-		return fmt.Sprintf("%s:", i)
-	}
-	output.FormatFieldValue = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("%s", i))
-	}
-	log.Logger = log.Output(output)
-	zerolog.SetGlobalLevel(getLevel(level))
-	zerolog.TimestampFunc = func() time.Time {
-		return time.Now().UTC()
-	}
+    }
+    output.FormatFieldName = func(i interface{}) string {
+        return fmt.Sprintf("%s:", i)
+    }
+    output.FormatFieldValue = func(i interface{}) string {
+        return strings.ToUpper(fmt.Sprintf("%s", i))
+    }
+    log.Logger = log.Output(output)
+    zerolog.SetGlobalLevel(getLevel(level))
+    zerolog.TimestampFunc = func() time.Time {
+        return time.Now().UTC()
+    }
 }
--- main.go
+++ main.go
@@ -1,68 +1,68 @@
 package main
 
 import (
-	"io/ioutil"
-	"strconv"
-	"strings"
+    "io/ioutil"
+    "strconv"
+    "strings"
 
-	"github.com/BurntSushi/toml"
-	"github.com/eoscanada/eos-go/ecc"
-	"github.com/kelseyhightower/envconfig"
+    "github.com/BurntSushi/toml"
+    "github.com/eoscanada/eos-go/ecc"
+    "github.com/kelseyhightower/envconfig"
 
-	"github.com/rs/zerolog/log"
+    "github.com/rs/zerolog/log"
 )
 
 
 type Config struct {
-	Server struct {
-		Port int `envconfig:"SERVER_PORT"`
-		LogLevel string `envconfig:"LOG_LEVEL"`
-	}
-	Broker struct {
-		TopicOffsetPath string `envconfig:"OFFSET_PATH""`
-	}
-	BlockChain struct {
-		PrivateKeyPath string `envconfig:"PRIVATEKEY_PATH"`
-	}
+    Server struct {
+        Port int `envconfig:"SERVER_PORT"`
+        LogLevel string `envconfig:"LOG_LEVEL"`
+    }
+    Broker struct {
+        TopicOffsetPath string `envconfig:"OFFSET_PATH""`
+    }
+    BlockChain struct {
+        PrivateKeyPath string `envconfig:"PRIVATEKEY_PATH"`
+    }
 }
 
 func readWIF(filename string) *ecc.PrivateKey {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Panic().Msg(err.Error())
-	}
-	wif := strings.TrimSpace(strings.TrimSuffix(string(content), "\n"))
-	pk, err := ecc.NewPrivateKey(wif)
-	if err != nil {
-		log.Panic().Msg(err.Error())
-	}
-	return pk
+    content, err := ioutil.ReadFile(filename)
+    if err != nil {
+        log.Panic().Msg(err.Error())
+    }
+    wif := strings.TrimSpace(strings.TrimSuffix(string(content), "\n"))
+    pk, err := ecc.NewPrivateKey(wif)
+    if err != nil {
+        log.Panic().Msg(err.Error())
+    }
+    return pk
 }
 
 
 func readConfigFile(cfg *Config) {
-	_, err  := toml.DecodeFile("config.toml", &cfg)
-	if err != nil {
-		log.Panic().Msg(err.Error())
-	}
+    _, err  := toml.DecodeFile("config.toml", &cfg)
+    if err != nil {
+        log.Panic().Msg(err.Error())
+    }
 }
 
 func readEnv(cfg *Config) {
-	err := envconfig.Process("", cfg)
-	if err != nil {
-		log.Panic().Msg(err.Error())
-	}
+    err := envconfig.Process("", cfg)
+    if err != nil {
+        log.Panic().Msg(err.Error())
+    }
 }
 
 func getAddr(port int) string {
-	return ":" + strconv.Itoa(port)
+    return ":" + strconv.Itoa(port)
 }
 
 func main() {
-	app := App{}
-	cfg := Config{}
-	readConfigFile(&cfg)
-	readEnv(&cfg)
-	app.Initialize(readWIF(cfg.BlockChain.PrivateKeyPath), cfg.Broker.TopicOffsetPath, cfg.Server.LogLevel)
-	app.Run(getAddr(cfg.Server.Port))
+    app := App{}
+    cfg := Config{}
+    readConfigFile(&cfg)
+    readEnv(&cfg)
+    app.Initialize(readWIF(cfg.BlockChain.PrivateKeyPath), cfg.Broker.TopicOffsetPath, cfg.Server.LogLevel)
+    app.Run(getAddr(cfg.Server.Port))
 }
